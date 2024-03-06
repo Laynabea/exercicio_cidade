c14002512290c01ea8378345aeb4012d75e0cbe3
Laura Beatriz <laura_b_moraes@estudante.sesisenai.org.br>
2024-03-06 15:34:09 -0300

    soma de arrays no back end

---
+++ atividade.06.07.array/principal_att.js
@@ -0,0 +1,20 @@
+const readlineSync = require('readline-sync')
+
+let numArray1 = [ ]
+let numArray2 = [ ]
+let numArray3 = [ ]
+
+
+let valor = 0 
+
+function subtrair(){
+
+}
+
+function ler(){
+
+}
+
+function imprimir(){
+    
+}
